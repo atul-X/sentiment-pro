653adaa8ef2004d495579f3b54086c43488967a9
Atul Kumar <atulkumar@Atul-KumarA2442JJ4R71V2071958.local>
2025-09-27 12:01:03 +0530

    logging added

---
+++ setup.py
@@ -0,0 +1,9 @@
+import nltk
+
+# Download necessary NLTK data packages for the application
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+print("NLTK data packages downloaded successfully.")
