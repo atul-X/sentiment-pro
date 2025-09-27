f06fd95302cbc99ed004943da7cb1854a322ddfe
Atul Kumar <atulkumar@Atul-KumarA2442JJ4R71V2071958.local>
2025-09-27 12:06:47 +0530

    logging added

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 import nltk
+import os
 
-# Download necessary NLTK data packages for the application
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+# Define the download directory, which corresponds to the nltk.cfg file
+download_dir = './nltk_data'
 
-print("NLTK data packages downloaded successfully.")
+# Create the directory if it doesn't exist
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
+# List of packages to download
+packages = ['punkt', 'stopwords', 'wordnet', 'omw-1.4', 'punkt_tab']
+
+# Download necessary NLTK data packages to the specified directory
+for package in packages:
+    try:
+        nltk.data.find(f'tokenizers/{package}')
+    except LookupError:
+        nltk.download(package, download_dir=download_dir)
+
+print(f"NLTK data packages downloaded successfully to {download_dir}.")
+
